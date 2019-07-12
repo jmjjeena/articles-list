45f0573f49717b05dd195e5c8a5a90e647f39991
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-07-11 19:16:35 -0700

    Add compose or using Redux Dev Tools together with other middlewares

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
 
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(forbiddenWordsMiddleware)
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
 );
 
 export default store;
